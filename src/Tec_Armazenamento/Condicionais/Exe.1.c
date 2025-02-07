f7fe0377e1058cc8d637f6df445b0ce413da6ffd
Guilherme <guilhermedelimadearaujo@gmail.com>
2025-02-07 19:33:35 -0300

    update

---
+++ src/Tec_Armazenamento/Condicionais/Exe.1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    double nota1, nota2, notaFinal;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+
+    printf("Digite a seginda nota: ");
+    scanf("%lf", &nota2);
+
+    notaFinal = nota1 + nota2;
+
+    if (notaFinal < 60.00){
+        printf("NOTA FINAL: %.1lf \n", notaFinal);
+        printf("REPROVADO");
+    }
+    else{
+        printf("NOTA FINAL: %.1lf \n", notaFinal);
+        printf("APROVADO");
+    }
+    return 0;
+}
---
+++ src/Tec_Armazenamento/Exe.8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    double A, B, C, areaQuadrado, areaTriangulo, areaTrapezio;
+
+    printf("Digite a medida A: ");
+    scanf("%lf", &A);
+
+    printf("Digite a medida B: ");
+    scanf("%lf", &B);
+
+    printf("Digite a medida C: ");
+    scanf("%lf", &C);
+
+    areaQuadrado = A * A;
+
+    printf("AREA DO QUADRADO: %.4lf \n", areaQuadrado);
+
+    areaTriangulo = (A * B) / 2;
+
+    printf("AREA DO TRIANGULO: %.4lf \n", areaTriangulo);
+
+    areaTrapezio = (A +B) * C / 2;
+
+    printf("AREA DO TRAPEZIO: %.4lf \n", areaTrapezio);
+
+    return 0;
+}
